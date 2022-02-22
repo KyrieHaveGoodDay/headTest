a866ba7596aa0cd0da7a4c7f010cf8f13554d9be
kyriehavegoodday <healthymbear@gmail.com>
2022-02-22 12:03:23 +0800

    從js渲染資料

---
+++ header.js
@@ -0,0 +1,30 @@
+let header = document.getElementById('header');
+let str = '';
+
+str = `
+<div class="header_main">
+    <div class="header1"></div>
+    <div class="header2"></div>
+    <div class="text_main">
+        <span class="head_text1"></span>
+        <span class="head_text2"></span>
+        <span class="head_text3"></span>
+    </div>
+    <div class="img_main">
+        <a href="">
+            <div class="img1"></div>
+        </a>
+        <a href="">
+            <div class="img2"></div>
+        </a>
+        <a href="">
+            <div class="img3"></div>
+        </a>
+        <a href="">
+            <div class="img4"></div>
+        </a>
+    </div>
+</div>
+
+`
+header.innerHTML = str;
